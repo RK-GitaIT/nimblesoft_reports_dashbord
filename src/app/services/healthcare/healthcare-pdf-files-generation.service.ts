12315d3d953652a79811d0d69d396c4dc4f952c5
RKGitaIT <rampolineni@gitait.com>
2025-03-07 20:11:39 +0530

    changes on pdf generation

--- src/app/services/healthcare/healthcare-pdf-files-generation.service.ts
+++ src/app/services/healthcare/healthcare-pdf-files-generation.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { PDFDocument } from 'pdf-lib';
+import { LegalDocumentsService } from '../leagl_documents/leagl-documents.service';
+import { ToastService } from '../../services/toast.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -7,91 +10,84 @@ import { PDFDocument } from 'pdf-lib';
 export class HealthcarePdfFilesGenerationService {
 
   client_data: any = null;
-  //files: File
-  constructor() {
-   // this.loadPdf
-   }
+  files: File[] = [];
 
-    private async medicalPowerOfAttorney(): Promise<void> {
-      try {
-        
-        const url = 'assets/pdf/MedicalPowerOfAttorney.pdf';
-        const pdfBytes = await fetch(url).then(res => res.arrayBuffer());
-        var pdfDoc = await PDFDocument.load(pdfBytes);
+  constructor(
+    private fileupload: LegalDocumentsService,
+    private toastService: ToastService ,
+    private router: Router
+  ) { }
 
-        const form = pdfDoc.getForm();
-        const availableFields = form.getFields().map(field => field.getName());
-
-       // console.log(availableFields);
-        // var fields.forEach(fieldDef => {
-        //   if("field35" == fieldDef.controlName){
-        //     console.log("log");
-        //   }
-        //   //const value = fieldDef.controlName;
-        //   const value = this.form.get(fieldDef.controlName)?.value;
-        //   if (availableFields.includes(fieldDef.pdfField)) {
-        //     try {
-        //       switch (fieldDef.type) {
-        //         case 'text':
-        //           form.getTextField(fieldDef.pdfField).setText(value || '');
-        //           break;
-        //         case 'radio':
-        //           form.getRadioGroup(fieldDef.pdfField).select(value);
-        //           break;
-        //         case 'checkbox':
-        //           const checkbox = form.getCheckBox(fieldDef.pdfField);
-        //           value ? checkbox.check() : checkbox.uncheck();
-        //           break;
-        //         default:
-        //           form.getTextField(fieldDef.pdfField).setText(value || '');
-        //       }
-        //       console.log(`Filled "${fieldDef.pdfField}" with value "${value}"`);
-        //     } catch (e) {
-        //       console.warn(`Error setting value for field "${fieldDef.pdfField}":`, e);
-        //     }
-        //   } else {
-        //     console.warn(`PDF field "${fieldDef.pdfField}" not found. Available fields: ${availableFields.join(', ')}`);
-        //   }
-        // });
-
-        console.log('PDF loaded successfully.');
-      } catch (error) {
-        console.error('Error loading PDF:', error);
-      }
+  private async medicalPowerOfAttorney(): Promise<void> {
+    try {
+      const url = 'assets/pdf/MedicalPowerOfAttorney.pdf';
+      const pdfBytes = await fetch(url).then(res => res.arrayBuffer());
+      const pdfDoc = await PDFDocument.load(pdfBytes);
+      const pdf_Bytes = await pdfDoc.save();
+      const blob = new Blob([pdf_Bytes], { type: 'application/pdf' });
+      const fileName = 'Medical power of attorney designation of health care agent.pdf';
+      const file = new File([blob], fileName, { type: 'application/pdf' });
+      this.files.push(file);
+      console.log('Medical Power of Attorney PDF loaded successfully.');
+    } catch (error) {
+      console.error('Error loading Medical Power Of Attorney PDF:', error);
     }
+  }
 
-    private async hipaa(): Promise<void> {
-      try {
-        
-        const url = 'assets/pdf/HiPa.pdf';
-        const pdfBytes = await fetch(url).then(res => res.arrayBuffer());
-        var pdfDoc = await PDFDocument.load(pdfBytes);
-
-        console.log('PDF loaded successfully.');
-      } catch (error) {
-        console.error('Error loading PDF:', error);
-      }
+  private async hipaa(): Promise<void> {
+    try {
+      const url = 'assets/pdf/HiPa.pdf';
+      const pdfBytes = await fetch(url).then(res => res.arrayBuffer());
+      const pdfDoc = await PDFDocument.load(pdfBytes);
+      const pdf_Bytes = await pdfDoc.save();
+      const blob = new Blob([pdf_Bytes], { type: 'application/pdf' });
+      const fileName = 'HIPAA.pdf';
+      const file = new File([blob], fileName, { type: 'application/pdf' });
+      this.files.push(file);
+      console.log('HIPAA PDF loaded successfully.');
+    } catch (error) {
+      console.error('Error loading HIPAA PDF:', error);
     }
+  }
 
-    private async sdpoa(): Promise<void> {
-      try {
-        
-        const url = 'assets/pdf/SDPOA.pdf';
-        const pdfBytes = await fetch(url).then(res => res.arrayBuffer());
-        var pdfDoc = await PDFDocument.load(pdfBytes);
-
-        console.log('PDF loaded successfully.');
-      } catch (error) {
-        console.error('Error loading PDF:', error);
-      }
+  private async sdpoa(): Promise<void> {
+    try {
+      const url = 'assets/pdf/SDPOA.pdf';
+      const pdfBytes = await fetch(url).then(res => res.arrayBuffer());
+      const pdfDoc = await PDFDocument.load(pdfBytes);
+      const pdf_Bytes = await pdfDoc.save();
+      const blob = new Blob([pdf_Bytes], { type: 'application/pdf' });
+      const fileName = 'SDPOA.pdf';
+      const file = new File([blob], fileName, { type: 'application/pdf' });
+      this.files.push(file);
+      console.log('SDPOA PDF loaded successfully.');
+    } catch (error) {
+      console.error('Error loading SDPOA PDF:', error);
     }
+  }
 
-    async loadPdfs(client_data: any)
-    {
-      this.client_data =  client_data;
-      await this.medicalPowerOfAttorney();
-      await this.hipaa();
-      await this.sdpoa();
-    }
+  async loadPdfs(client_data: any): Promise<void> {
+    this.client_data = client_data;
+    this.files = [];
+    await this.medicalPowerOfAttorney();
+    await this.hipaa();
+    await this.sdpoa();
+    await this.pdf_files_uploads();
+  }
 
+  private async pdf_files_uploads(): Promise<void> {
+
+    this.fileupload.uploadDocuments(this.files, "Healthcare", "Healthcare", 1)
+      .subscribe({
+        next: (response) => {
+          console.log("PDFs uploaded successfully:", response);
+          this.toastService.showToast('Files created successfully!', '', 'success', 3000);
+          this.router.navigate(['/my-files']);
+        },
+        error: (error) => {
+          console.error("Error uploading PDFs:", error);
+          this.toastService.showToast('Error creating PDFs', '', 'error', 3000);
+        }
+      });
+  }
 }
