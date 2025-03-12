5cee2efe2376d386726f97f279dc9097e1d5deb5
Chaitanya Seethalam <chaitanya.s@gitait.com>
2025-03-12 20:43:19 +0530

    Added the options component

--- src/app/component/smart-services/utilities/option-selection/option-selection.component.ts
+++ src/app/component/smart-services/utilities/option-selection/option-selection.component.ts
@@ -1,120 +1,72 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Beneficiary } from '../../../../models/interfaces/Beneficiary.model';
+
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Beneficiary } from '../../../../models/interfaces/Beneficiary.model';
+import { OptionSelectionData } from '../../../../models/interfaces/utilities/IOptionSelectionData';
 
 @Component({
   selector: 'app-option-selection',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule,FormsModule],
   templateUrl: './option-selection.component.html',
   styleUrl: './option-selection.component.css'
 })
 export class OptionSelectionComponent implements OnInit {
 
-  @Input() data_members: Beneficiary[] = [];
-  @Input() DocumentPrepareFor: DocumentPrepareFor | null = null;
-  @Output() selectionConfirmed = new EventEmitter<Beneficiary[]>();
-  @Output() selectionCanceled = new EventEmitter<void>();
+  @Input() optionSelectionData!: OptionSelectionData;
+  @Output() selectionConfirmed = new EventEmitter<string>();
+  @Output() selectionCanceled = new EventEmitter<string>();
+  @Output() selectedMembers = new EventEmitter<Beneficiary[]>
 
-  successorType: string = 'multiple';
+  selectionType: 'multiple' | 'one' | 'none' = 'multiple'; // Default selection type
 
   ngOnInit(): void {
-    console.log('HIPAA Auth Representatives – Loaded Members:', this.data_members);
-    console.log('HIPAA Auth Representatives – DocumentPrepareFor:', this.DocumentPrepareFor);
+    console.log('Option Selection Loaded:', this.optionSelectionData);
   }
 
-  /**
-   * Called whenever the dropdown changes. We automatically adjust the selection
-   * array if "none" or "one" is chosen.
-   */
-  onSuccessorTypeChange(newVal: string): void {
-    this.successorType = newVal;
-    if (!this.DocumentPrepareFor) return;
+  onSelectionTypeChange(newType: 'multiple' | 'one' | 'none'): void {
+    this.selectionType = newType;
 
-    if (this.successorType === 'none') {
-      // Clear any previously selected reps
-      this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives = [];
-    } else if (this.successorType === 'one') {
-      // If more than one was selected, keep only the first
-      const selected = this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives;
-      if (selected.length > 1) {
-        this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives = [selected[0]];
-      }
+    if (newType === 'none') {
+      this.optionSelectionData.selectedMembers = [];
+    } else if (newType === 'one' && this.optionSelectionData.selectedMembers.length > 1) {
+      this.optionSelectionData.selectedMembers = [this.optionSelectionData.selectedMembers[0]];
     }
   }
 
-  /**
-   * Toggle selection of a user. Behavior depends on successorType:
-   * - "multiple": standard toggle (add/remove)
-   * - "one": only one user can be selected at a time
-   * - "none": no one can be selected
-   */
-  toggleSelection(user: Beneficiary): void {
-    if (!this.DocumentPrepareFor) return;
+  toggleSelection(user: any): void {
+    if (this.selectionType === 'none') return;
 
-    // If "none", ignore any clicks
-    if (this.successorType === 'none') {
-      return;
-    }
-
-    // If "one", we only keep zero or one
-    if (this.successorType === 'one') {
-      const list = this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives;
-      if (list.length === 1) {
-        const [existing] = list;
-        // If clicking the same user, remove
-        if (existing.index === user.index) {
-          this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives = [];
-        } else {
-          // Otherwise replace with the new user
-          this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives = [user];
-        }
-      } else {
-        // If none or more than one, just set the array to the clicked user
-        this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives = [user];
-      }
-      return;
-    }
-
-    // If "multiple", do the standard toggle
-    const index = this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives
-      .findIndex(selected => selected.index === user.index);
-
-    if (index > -1) {
-      // Already selected, remove
-      this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives.splice(index, 1);
+    if (this.selectionType === 'one') {
+      this.optionSelectionData.selectedMembers = [user];
     } else {
-      // Not yet selected, add
-      this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives.push(user);
+      const index = this.optionSelectionData.selectedMembers.findIndex((selected) => selected.index === user.index);
+      if (index > -1) {
+        this.optionSelectionData.selectedMembers.splice(index, 1);
+      } else {
+        this.optionSelectionData.selectedMembers.push(user);
+      }
     }
   }
-
-  /**
-   * When Next is clicked, emit the final selection array
-   */
-  confirmToNext(): void {
-    if (!this.DocumentPrepareFor) return;
-    console.log('Selected HIPAA Representatives:', 
-      this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives
-    );
-    this.selectionConfirmed.emit(
-      [...this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives]
-    );
+  getUserColor(user: { firstName: string; lastName: string }): string {
+    const colors = [
+      'bg-red-500', 'bg-blue-500', 'bg-green-500', 'bg-yellow-500',
+      'bg-purple-500', 'bg-pink-500', 'bg-indigo-500', 'bg-teal-500'
+    ];
+    const index = (user.firstName.charCodeAt(0) + user.lastName.charCodeAt(0)) % colors.length;
+    return colors[index];
+  }
+  isSelected(user: any): boolean {
+    return this.optionSelectionData.selectedMembers.some((selected) => selected.index === user.index);
   }
 
-  /**
-   * When Back is clicked, emit a cancellation event
-   */
+  confirmSelection(): void {
+    this.selectionConfirmed.emit(this.optionSelectionData.next);
+    this.selectedMembers.emit(this.optionSelectionData.selectedMembers);
+  }
+
   cancelSelection(): void {
-    this.selectionCanceled.emit();
-  }
-
-  /**
-   * Returns true if the user is currently selected
-   */
-  isSelected(user: Beneficiary): boolean {
-    return this.DocumentPrepareFor 
-      ? this.DocumentPrepareFor.HealthcareHipaaAuthorizationSuccessorRepresentatives
-        .some(selected => selected.index === user.index)
-      : false;
+    this.selectionCanceled.emit(this.optionSelectionData.back);
   }
 }
