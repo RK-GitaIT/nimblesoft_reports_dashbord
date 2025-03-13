d5a8b539b836c07a0fc5ddf02d00e4eef9e0ef5b
RKGitaIT <rampolineni@gitait.com>
2025-03-13 21:37:20 +0530

    changes on flags

--- src/app/models/interfaces/utilities/IPersonalResidence.ts
+++ src/app/models/interfaces/utilities/IPersonalResidence.ts
@@ -1,13 +1,15 @@
 import { Beneficiary } from "../Beneficiary.model";
 import { IRequests } from "./IRequests";
 
-export interface IPersonalResidence{
-  PersonalResidenceDevise?: boolean,
-  Beneficiary?: Beneficiary[],
-  Effective_date_of_devise?: string,
-  Replacement_property?: boolean,
-  beneficiaries_requests?: IRequests,
-  finalData?: any, 
-  back: string,
-  next: string,
-}
+export interface IPersonalResidence {
+    PersonalResidenceDevise?: boolean;
+    Beneficiary?: Beneficiary[];
+    Effective_date_of_devise?: string;
+    Replacement_property?: boolean;
+    beneficiaries_requests?: any;
+    finalData?: any;
+    ownershipType?: string; 
+    back: string;
+    next: string;
+  }
+  
